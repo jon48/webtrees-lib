5a650d3d7cef6f734a3e4ddaf00f70fee2b8408a
Jonathan Jaubart <dev@jaubart.com>
2023-02-12 09:25:54 +0000

    Update copyright year to 2023
    (cherry picked from commit 386456f1202ef6f21dd5dcc363720b17fdd4f86c)

--- app/Module/PatronymicLineage/Model/LineageBuilder.php
+++ app/Module/PatronymicLineage/Model/LineageBuilder.php
@@ -6,7 +6,7 @@
  * @package MyArtJaub\Webtrees
  * @subpackage PatronymicLineage
  * @author Jonathan Jaubart <dev@jaubart.com>
- * @copyright Copyright (c) 2009-2022, Jonathan Jaubart
+ * @copyright Copyright (c) 2009-2023, Jonathan Jaubart
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
  */
 
